f7c613e5d5a19fa84f1f6a1743d872163d6342b7
Johan Litsfeldt <johan.litsfeldt@uu.se>
2022-12-01 14:42:57 +0100

    day 1

---
+++ aoc2022/day2/Day2.cs
@@ -0,0 +1,17 @@
+﻿public class Day2
+{
+    public static void Run()
+    {
+        using (StreamReader file = new StreamReader("day2/p.in"))
+        {
+            string ln;
+            while ((ln = file.ReadLine()) != null)
+            {
+                Console.WriteLine(ln);
+            }
+
+            file.Close();
+        }
+    }
+}
+
