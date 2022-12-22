b5a055f5c7b6eb73faf45dedc2bc1aabc8b96d34
Johan Litsfeldt <johan.litsfeldt@uu.se>
2022-12-22 14:42:30 +0100

    Day 22

--- aoc2022/day22/Day22.cs
+++ aoc2022/day22/Day22.cs
@@ -1,4 +1,6 @@
-﻿public class Day22
+﻿using System.Runtime.InteropServices;
+
+public class Day22
 {
     private static int w = 150;
     private static int h = 200;
@@ -7,42 +9,82 @@
     //private static int h = 12;
     //private static char[,] map = new char[16, 12];
     private static int side = 1;
+    private static char turn = 'R';
+    private static int actualside = 1;
+    private static char actualturn = 'R';
+    private static char origturn = 'R';
 
     private static Tuple<int, int> MoveRight(int x, int y)
     {
-        if (side == 1)
+        if (side == 1 || side == 2 || side == 3)
         {
             x++;
         }
+        if (side == 4 || side == 5)
+        {
+            x--;
+        }
+        if (side == 6)
+        {
+            y++;
+        }
+        //x++;
         
         return new Tuple<int, int>(x, y);
     }
 
     private static Tuple<int, int> MoveLeft(int x, int y)
     {
-        if (side == 1)
+        if (side == 1 || side == 2 || side == 3)
         {
             x--;
         }
+        if (side == 4 || side == 5)
+        {
+            x++;
+        }
+        if (side == 6)
+        {
+            y--;
+        }
+        //x--;
         return new Tuple<int, int>(x, y);
     }
 
     private static Tuple<int, int> MoveUp(int x, int y)
     {
-        if (side == 1)
+        if (side == 1 || side == 2 || side == 3 )
         {
             y--;
         }
+        if (side == 4 || side == 5)
+        {
+            y++;
+        }
+        if (side == 6)
+        {
+            x++;
+        }
+        //y--;
         return new Tuple<int, int>(x, y);
     }
 
     private static Tuple<int, int> MoveDown(int x, int y)
     {
-        if (side == 1)
+        if (side == 1 || side == 2 || side == 3)
         {
             y++;
         }
-        
+        if (side == 4 || side == 5)
+        {
+            y--;
+        }
+        if (side == 6)
+        {
+            x--;
+        }
+        //y++;
+
         return new Tuple<int, int>(x, y);
     }
 
@@ -50,38 +92,228 @@
     {
         var x = pos.Item1;
         var y = pos.Item2;
-        if (x == w) x = 0;
-        if (x == -1) x = w - 1;
-        if (y == -1) y = h - 1;
-        if (y == h) y = 0;
+
+        if (side == 1)
+        {
+            if (x == 49)
+            {
+                side = 4;
+                y = 150 - y - 1;
+                x = 0;
+            }
+            else if (x == 100)
+            {
+                side = 2;
+            }
+            else if (y == -1)
+            {
+                side = 6;
+                y = 150 + x - 50;
+                x = 0;
+            }
+            else if (y == 50)
+            {
+                side = 3;
+            }
+        }
+        else if (side == 2)
+        {
+            if (x == 99)
+            {
+                side = 1;
+            }
+            else if (x == 150)
+            {
+                side = 5;
+                x = 99;
+                y = 150 - y - 1;
+            }
+            else if (y == -1)
+            {
+                side = 6;
+                y = 199;
+                x = x - 100;
+            }
+            else if (y == 50)
+            {
+                side = 3;
+                y = 50 + x - 100;
+                x = 99;
+            }
+        }
+        else if (side == 3)
+        {
+            if (y == 49)
+            {
+                side = 1;
+            }
+            else if (y == 100)
+            {
+                side = 5;
+            }
+            else if (x == 49)
+            {
+                side = 4;
+                x = y - 50;
+                y = 100;
+            }
+            else if (x == 100)
+            {
+                side = 2;
+                x = 100 + y - 50;
+                y = 49;
+            }
+        }
+        else if (side == 4)
+        {
+            if (x == -1)
+            {
+                side = 1;
+                y = 50 - (y - 100) - 1;
+                x = 50;
+            }
+            else if (x == 50)
+            {
+                side = 5;
+            }
+            else if (y == 99)
+            {
+                side = 3;
+                y = 50 + x;
+                x = 50;
+            }
+            else if (y == 150)
+            {
+                side = 6;
+            }
+        }
+        else if (side == 5)
+        {
+            if (x == 49)
+            {
+                side = 4;
+            }
+            else if (x == 100)
+            {
+                side = 2;
+                y = 50 - (y - 100) - 1;
+                x = 149;
+            }
+            else if (y == 99)
+            {
+                side = 3;
+            }
+            else if (y == 150)
+            {
+                side = 6;
+                y = 150 + (x - 50);
+                x = 49;
+            }
+        }
+        else if (side == 6)
+        {
+            if (x == -1)
+            {
+                side = 1;
+                x = 50 + (y - 150);
+                y = 0;
+            }
+            else if (x == 50) 
+            {
+                side = 5;
+                x = 50 + y - 150;
+                y = 149;
+            }
+            else if (y == 149)
+            {
+                side = 4;
+            }
+            else if (y == 200)
+            {
+                side = 2;
+                x = 100 + x;
+                y = 0;
+            }
+        }
+
+        //if (x == w) x = 0;
+        //if (x == -1) x = w - 1;
+        //if (y == -1) y = h - 1;
+        //if (y == h) y = 0;
         return new Tuple<int, int>(x, y);
     }
 
-    private static Tuple<int, int> DoMove(Tuple<int, int> pos, char turn)
+    private static Tuple<int, int> DoMove(Tuple<int, int> pos)
     {
+        side = actualside;
+        turn = actualturn;
+
         Tuple<int, int> newPos = null;
         if (turn == 'R') newPos = Wrap(MoveRight(pos.Item1, pos.Item2));
-        if (turn == 'L') newPos = Wrap(MoveLeft(pos.Item1, pos.Item2));
-        if (turn == 'U') newPos = Wrap(MoveUp(pos.Item1, pos.Item2));
-        if (turn == 'D') newPos = Wrap(MoveDown(pos.Item1, pos.Item2));
+        else if (turn == 'L') newPos = Wrap(MoveLeft(pos.Item1, pos.Item2));
+        else if (turn == 'U') newPos = Wrap(MoveUp(pos.Item1, pos.Item2));
+        else if (turn == 'D') newPos = Wrap(MoveDown(pos.Item1, pos.Item2));
 
-        while (map[newPos.Item1, newPos.Item2] == ' ')
-        {
-            if (turn == 'R') newPos = Wrap(MoveRight(newPos.Item1, newPos.Item2));
-            if (turn == 'L') newPos = Wrap(MoveLeft(newPos.Item1, newPos.Item2));
-            if (turn == 'U') newPos = Wrap(MoveUp(newPos.Item1, newPos.Item2));
-            if (turn == 'D') newPos = Wrap(MoveDown(newPos.Item1, newPos.Item2));
-        }
+
+        //while (map[newPos.Item1, newPos.Item2] == ' ')
+        //{
+        //    if (turn == 'R') newPos = Wrap(MoveRight(newPos.Item1, newPos.Item2));
+        //    if (turn == 'L') newPos = Wrap(MoveLeft(newPos.Item1, newPos.Item2));
+        //    if (turn == 'U') newPos = Wrap(MoveUp(newPos.Item1, newPos.Item2));
+        //    if (turn == 'D') newPos = Wrap(MoveDown(newPos.Item1, newPos.Item2));
+        //}
+
+        Console.WriteLine("f " + pos.Item1 + " " + pos.Item2 + " (side " + actualside + ", dir " + turn + ")");
+        Console.WriteLine("t " + newPos.Item1 + " " + newPos.Item2 + " (side " + side + ")");
 
         if (map[newPos.Item1, newPos.Item2] == '#')
         {
+            //Console.WriteLine("blocked");
             return new Tuple<int, int>(pos.Item1, pos.Item2);
         }
         else if (map[newPos.Item1, newPos.Item2] == '.')
         {
+
+            if (actualside != side)
+            {
+                Console.WriteLine("f " + pos.Item1 + " " + pos.Item2 + " (side " + actualside + ", dir " + turn + ")");
+                Console.WriteLine("t " + newPos.Item1 + " " + newPos.Item2 + " (side " + side + ")");
+                Console.WriteLine();
+
+                
+                if (actualside == 2 && side == 3) turn = 'L';
+                if (actualside == 2 && side == 6) turn = 'L';
+
+                if (actualside == 3 && side == 2) turn = 'U';
+                if (actualside == 3 && side == 4) turn = 'U';
+                if (actualside == 3 && side == 5) turn = 'U';
+
+                if (actualside == 4 && side == 3) turn = 'R';
+                if (actualside == 4 && side == 6) turn = 'R';
+
+                if (actualside == 5 && side == 3) turn = 'U';
+                if (actualside == 5 && side == 6) turn = 'D';
+                
+                if (actualside == 6 && side == 2) turn = 'D';
+                if (actualside == 6 && side == 4) turn = 'D';
+                if (actualside == 6 && side == 5) turn = 'D';
+                
+                
+            }
+            else
+            {
+                //Console.WriteLine(newPos.Item1 + " " + newPos.Item2 + " (side " + side + ")");
+            }
+            //Console.WriteLine("OK");
+            actualside = side;
+            actualturn = turn;
             return newPos;
         }
-       
+
+        Console.WriteLine("WARNING");
+        Console.WriteLine("f " + pos.Item1 + " " + pos.Item2 + " (side " + actualside + ", dir " + turn + ")");
+        Console.WriteLine("t " + newPos.Item1 + " " + newPos.Item2 + " (side " + side + ")");
+        Console.WriteLine();
         throw new Exception();
     }
     public static void Run()
@@ -121,7 +353,6 @@
             }
 
             var str = "";
-            var turn = 'R';
             for(var c = 0; c < movement.Length; c++)
             {
                 if (movement[c] == 'R')
@@ -130,16 +361,17 @@
                         var m = int.Parse(str);
                         for (var move = 0; move < m; move++)
                         {
-                            pos = DoMove(pos, turn);
+                            pos = DoMove(pos);
                             //Console.WriteLine(pos.Item1 + " " + pos.Item2);
                         }
                         str = "";
                     }
                     // clockwise
-                    if (turn == 'R') { turn = 'D'; }
-                    else if (turn == 'D') { turn = 'L'; }
-                    else if (turn == 'L') { turn = 'U'; }
-                    else if (turn == 'U') { turn = 'R'; }
+                    if (actualturn == 'R') { actualturn = 'D'; }
+                    else if (actualturn == 'D') { actualturn = 'L'; }
+                    else if (actualturn == 'L') { actualturn = 'U'; }
+                    else if (actualturn == 'U') { actualturn = 'R'; }
+
                 }
                 else if (movement[c] == 'L')
                 {
@@ -148,15 +380,15 @@
                         var m = int.Parse(str);
                         for (var move = 0; move < m; move++)
                         {
-                            pos = DoMove(pos, turn);
+                            pos = DoMove(pos);
                         }
                         str = "";
                     }
                     // clockwise
-                    if (turn == 'R') { turn = 'U'; }
-                    else if (turn == 'D') { turn = 'R'; }
-                    else if (turn == 'L') { turn = 'D'; }
-                    else if (turn == 'U') { turn = 'L'; }
+                    if (actualturn == 'R') { actualturn = 'U'; }
+                    else if (actualturn == 'D') { actualturn = 'R'; }
+                    else if (actualturn == 'L') { actualturn = 'D'; }
+                    else if (actualturn == 'U') { actualturn = 'L'; }
                 }
                 else
                 {
@@ -169,19 +401,14 @@
                 var m = int.Parse(str);
                 for (var move = 0; move < m; move++)
                 {
-                    pos = DoMove(pos, turn);
+                    pos = DoMove(pos);
                 }
                 str = "";
             }
 
-            // Facing is 0 for right (>), 1 for down (v), 2 for left (<), and 3 for up (^)
             var row = pos.Item2 + 1;
             var col = pos.Item1 + 1;
-            var face = 0;
-            if (turn == 'D') face = 1;
-            if (turn == 'L') face = 2;
-            if (turn == 'U') face = 3;
-            var tot = row * 1000 + 4 * col + face;
+            var tot = row * 1000 + 4 * col + 2; // moving left in the end
             Console.WriteLine(row + " " + col);
             Console.WriteLine(tot);
 
